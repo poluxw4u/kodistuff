a29ea0f99c4c1f0f37093c126772a964aaa0ebfd
Jairox <jairoxyz@mailfence.com>
2017-06-21 22:21:47 +0800

    def

--- plugin.video.bonanza/default.py
+++ plugin.video.bonanza/default.py
@@ -1,7 +1,7 @@
 ﻿'''
     IPTV BONANZA
     Copyright (C) 2017 BONANZA
-    Based on Ultimate IPTV by  Whitecream - All credits to him!
+    Based on Ultimate IPTV by theBoss - All credits to him!
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
